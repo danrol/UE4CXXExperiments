3b433b9e50a1818ae97b806358894b503af73a1a
Daniel Rolnik <danrol92@gmail.com>
2020-09-21 20:57:23 +0500

    upd

---
+++ Section2/triplex.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "PicleRick!!!!"; //way to write to console
+  std::cout << std::endl; // add new line
+  std::cout << "You are Pickle rick";
+  std::cout << "dfsf";
+
+  return 0;
+}

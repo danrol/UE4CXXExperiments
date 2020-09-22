2381801c5e4872f25a195e587dc2d2828569e8aa
Daniel Rolnik <danrol92@gmail.com>
2020-09-22 20:41:11 +0500

    upd

--- Section2/triplex.cpp
+++ Section2/triplex.cpp
@@ -5,7 +5,20 @@ int main()
   std::cout << "PicleRick!!!!"; //way to write to console
   std::cout << std::endl; // add new line
   std::cout << "You are Pickle rick";
+  std::cout << std::endl;
   std::cout << "dfsf";
+
+  int a = 3; //variable declaration
+  int b = 3;
+  int c = 2;
+
+  int sum = a + b + c;
+  int prod = a * b *c;
+  std::cout << std::endl;
+  //std::cout << a; //print variable to console
+  //std::cout << a + b + c; // print sum of a + b + c
+  std::cout << sum << std::endl; //end line on the same line
+  std::cout << prod << std::endl;
 
   return 0;
 }

dd2f062515d132bcedaf4729f9523d7a8eaf023f
Daniel Rolnik <danrol92@gmail.com>
2020-09-23 18:57:19 +0500

    upd

--- Section2/triplex.cpp
+++ Section2/triplex.cpp
@@ -4,24 +4,38 @@ int main()
 {
   std::cout << "PicleRick!!!!"; //way to write to console
   std::cout << std::endl; // add new line
-  std::cout << "You are Pickle rick";
+  std::cout << "You are Pickle rick" << std::endl; // add new line at the end of the line
   std::cout << std::endl;
-  std::cout << "dfsf";
 
-  int a = 3; //variable declaration
-  int b = 3;
-  const int c = 2; //declare constant variable 
+  int CodeA = 3; //variable declaration
+  int CodeB = 3;
+  const int CodeC = 2; //declare constant variable 
 
 /* 
 multi line comment
 */
-  int sum = a + b + c;
-  int prod = a * b *c;
-  std::cout << std::endl;
+  int CodeSum = CodeA + CodeB + CodeC;
+  int CodeProduct = CodeA * CodeB *CodeC;
+  std::cout << std::endl; // add new line
   //std::cout << a; //print variable to console
   //std::cout << a + b + c; // print sum of a + b + c
-  std::cout << sum << std::endl; //end line on the same line
-  std::cout << prod << std::endl;
+  std::cout << "There are 3 numbers in the code\n";
+  std::cout << "The codes add-up to: " << CodeSum << std::endl;//add variable value at the end of the line
+  std::cout << "Product of those numbers is equal to " << CodeProduct << std::endl;
 
-  return 0;
+  int GuessA, GuessB, GuessC;
+  std::cout << "Enter first number: ";
+  std::cin >> GuessA; // get user's input from console
+  std::cout << "Enter second number: ";
+  std::cin >>GuessB;
+  std::cout << "Enter third number: ";
+  std::cin >> GuessC;
+
+  std::cout << "You entered: " << GuessA << " " << GuessB << " " << GuessC << std::endl;
+
+int GuessSum = GuessA + GuessB + GuessC;
+int GuessProduct = GuessA * GuessB * GuessC;
+
+bool IsGuessRight; // boolean value
+return 0;
 }

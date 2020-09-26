9889d8540f15f4b57207f71356d884cd92a4fd30
Daniel Rolnik <danrol92@gmail.com>
2020-09-26 09:18:57 +0500

    upd

--- Section3/BullCowGame/Source/BullCowGame/BullCowCartridge.cpp
+++ Section3/BullCowGame/Source/BullCowGame/BullCowCartridge.cpp
@@ -5,11 +5,21 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
     PrintLine(TEXT("Welcome to  Bull and Cows game!"));
+    PrintLine(TEXT("Guess the 4 letter word!")); // TODO remove hard code
     PrintLine(TEXT("Press enter to continue..."));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
-    FString HiddenWord =TEXT( "above");
+    FString HiddenWord =TEXT("above"); //TODO move outside this function
+    if(Input == HiddenWord)
+    {
+        PrintLine(TEXT("Win!!!!"));
+    }
+    else
+    {
+        PrintLine(TEXT("Loose"));
+    }
+    
 }

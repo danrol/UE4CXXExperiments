dbc7c96ebb36100b20eac99ce4c2bc26f25f3d40
Daniel Rolnik <danrol92@gmail.com>
2020-09-28 16:12:55 +0500

    upd

---
+++ Section3/BullCowGame/Source/BullCowGame/HiddenWordsList.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+const TArray<FString> HiddenWords =
+{
+TEXT("regulations"),
+TEXT("switzerland"),
+TEXT("personality"),
+TEXT("documentary"),
+TEXT("copyrighted"),
+TEXT("backgrounds"),
+TEXT("republicans"),
+TEXT("atmospheric"),
+TEXT("demographic"),
+TEXT("background"),
+TEXT("compatible")
+};
